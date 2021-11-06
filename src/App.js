056dd390db3d3170fe0a89ff0a4b4753154b2aeb
Billy Jacoby <billyjacoby@gmail.com>
2021-11-06 16:51:00 -0400

    session 1 complete

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ const TEST_GIFS = [
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [gifList, setGifList] = useState([]);
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -50,6 +53,14 @@ const App = () => {
     }
   };
 
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log("Gif link: ", inputValue);
+    } else {
+      console.log("Empty input, try again.");
+    }
+  };
+
   const renderNotConnectedContainer = () => (
     <button
       className="cta-button connect-wallet-button"
@@ -59,11 +70,43 @@ const App = () => {
     </button>
   );
 
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <input
+        type="text"
+        placeholder="Enter GIF link!"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button className="cta-button submit-gif-button" onClick={sendGif}>
+        Submit
+      </button>
+      <div className="gif-grid">
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   useEffect(() => {
     window.addEventListener("load", async (event) => {
       await checkIfWalletIsConnected();
     });
   }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log("Fetching GIF list...");
+
+      // Call program to get GIF list here
+
+      // Set the gif list state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
   return (
     <div className="App">
       <div className="container">
@@ -73,6 +116,7 @@ const App = () => {
             View your GIF collection in the metaverse ✨
           </p>
           {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
